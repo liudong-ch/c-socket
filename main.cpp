18b71f12d856ef03b0d0750b1da859b1a51c0e3f
Liu Dong <dong.liu@aqrose.com>
2023-04-26 18:24:14 +0800

    c++调试环境

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello,VScode!" << endl; 
+
+    cout << "Hello,VScode!" << endl;
+    cout << "Hello,VScode!" << endl;
+    system("pause");
+    return 0;
+}
