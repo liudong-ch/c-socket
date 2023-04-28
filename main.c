b96e372035e27dd0170179cb9dd3f29a39e40d95
liudong <liudong_ch@qq.com>
2023-04-28 09:56:35 +0800

    tasks

---
+++ main.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+// using namespace std;
+
+int main()
+{
+    // cout << "Hello,VScode!" << endl; 
+
+    // cout << "Hello,VScode!" << endl;
+    // cout << "Hello,VScode!" << endl;
+
+    printf("hello\n");
+    printf("hello\n");
+    printf("hello\n");
+    
+    return 0;
+}
